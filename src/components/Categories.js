fb9f28fe02444e82c48d7d36b81dbfdb0c2430f2
Blessiey <mogashoablessing676@gmail.com>
2024-08-05 09:19:16 +0200

    commit

---
+++ src/components/Categories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Category({ cat }) {
+    return (
+        <div>{cat}</div>
+    )
+}
+export default Category
--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { NewsContext } from '../NewsContext'
+import { NewsContext } from './NewsContext'
 import NewsArticle from './NewsArticle';
 
 
